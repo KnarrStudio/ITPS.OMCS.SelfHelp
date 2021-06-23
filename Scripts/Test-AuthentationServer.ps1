7be2df8f8a7ea96022c0f444c35b7b3b1a75ff36
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-06-22 20:38:15 -0500

    Update Test-AuthentationServer.ps1

--- Scripts/Test-AuthentationServer.ps1
+++ Scripts/Test-AuthentationServer.ps1
@@ -1,5 +1,21 @@
-﻿function Test-AuthentationServer
+﻿#requires -Version 1.0
+
+function Test-AuthentationServer
 {
+  <#
+      .SYNOPSIS
+      Checks to see if the local machine is part of a domain
+
+      .DESCRIPTION
+      Add a more complete description of what the function does.
+
+      .EXAMPLE
+      Test-AuthentationServer
+      Looks for an Domain controller and displays it
+
+  #>
+
+
   try
   {
     # Check if computer is connected to domain network
@@ -12,4 +28,4 @@
     Write-Output -InputObject ('Local Workstation: {0}' -f $env:COMPUTERNAME)
   }
 }
-    
+Test-AuthentationServer
