6c7c94ff0f9b688d775b3feecc1ef07e063f79c7
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-07-13 09:52:35 -0500

    Create Add-AddOnsSelfHelpMenu.ps1

---
+++ Scripts/Add-AddOnsSelfHelpMenu.ps1
@@ -0,0 +1,45 @@
+﻿
+# .Add("Title of Menu",{Scriptblock},"HotKeys 'Ctrl+Alt+B'")
+
+# Create the Menu Object
+$MenuObject = $psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Add('Self Help',$null,$null) 
+
+
+# Create the Submenu Object
+$MenuObject.Submenus.Add('Test the Internet',{
+    C:\Users\erika\Documents\GitHub\ITPS-SelfHelp\Scripts\Test-TheInternet.ps1
+},'Ctrl+Alt+T')   
+$MenuObject.Submenus.Add('Compare File Hash',{
+    C:\Users\erika\Documents\GitHub\ITPS-SelfHelp\Scripts\Compare-FileHash.ps1
+},'Ctrl+Alt+F')
+$MenuObject.Submenus.Add('Test Authentication Server',{
+    C:\Users\erika\Documents\GitHub\ITPS-SelfHelp\Scripts\Test-AuthentationServer.ps1
+},'Ctrl+Alt+A')
+
+
+# Clear the AddOnsMenu
+#$MenuObject.AddOnsMenu.Clear() 
+
+# Clear the Submenu
+#$MenuObject.Submenus.Clear() 
+
+
+########################################
+# Clear the Add-ons menu if one exists.
+#$psISE.CurrentPowerShellTab.AddOnsMenu.SubMenus.Clear()
+
+# Create an AddOns menu with an accessor.
+# Note the use of "_"  as opposed to the "&" for mapping to the fast key letter for the menu item.
+#$menuAdded = $psISE.CurrentPowerShellTab.AddOnsMenu.SubMenus.Add('_Process', {Get-Process}, 'Alt+P')
+
+# Add a nested menu.
+#$parentAdded = $psISE.CurrentPowerShellTab.AddOnsMenu.SubMenus.Add('Parent', $null, $null)
+#$parentAdded.SubMenus.Add('_Dir', {dir}, 'Alt+D')
+
+# Show the Add-ons menu on the current PowerShell tab.
+#$psISE.CurrentPowerShellTab.AddOnsMenu
+
+
+
+
+
