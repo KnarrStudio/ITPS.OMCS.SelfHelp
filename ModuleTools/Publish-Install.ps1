dfdff3534ce3968775210d6b1bedd72c50ee14d8
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-07-31 07:46:52 -0500

    Create Publish-Install.ps1

---
+++ ModuleTools/Publish-Install.ps1
@@ -0,0 +1,34 @@
+﻿#!/usr/bin/env powershell
+#requires -Version 2.0 -Modules PowerShellGet
+
+$PSGallery = 'LocalPSGallery'
+$ModuleName = 'ITPS-SelfHelp'
+$ModuleLocation = 'd:/GitHub/KnarrStudio/'
+
+Set-Location -Path $ModuleLocation
+
+$PublishSplat = @{
+  Name       = ('{0}\{1}' -f $ModuleLocation, $ModuleName)
+  Repository = $PSGallery
+}
+
+$InstallSplat = @{
+ Name = $ModuleName
+ Repository = $PSGallery
+ Scope = 'CurrentUser' 
+ AllowClobber = $true
+ Force = $true
+ }
+
+
+
+Publish-Module @PublishSplat 
+Install-Module @InstallSplat
+
+
+
+
+
+
+
+
