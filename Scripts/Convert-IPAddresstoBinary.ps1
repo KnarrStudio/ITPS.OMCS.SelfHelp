dc11ce16bdf1765b834422635b1a4c89bb906ef3
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-11-23 21:26:10 -0600

    Update Convert-IPAddresstoBinary.ps1

--- Scripts/Convert-IPAddresstoBinary.ps1
+++ Scripts/Convert-IPAddresstoBinary.ps1
@@ -2,7 +2,7 @@
  
   param
   (
-    [Parameter(Mandatory,HelpMessage='IP v4 as "192.168.10.25"')]
+    [Parameter(Mandatory,HelpMessage='v4 IP Address as "192.168.10.25"')]
     [String]$dottedDecimal
   )
 $dottedDecimal.split('.') | ForEach-Object{$binary=$binary + $([convert]::toString($_,2).padleft(8,'0'))}
